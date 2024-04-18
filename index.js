3f4d8f09b9994a489a22ade6bd93dbe601d518a4
Paula_Maureira <paumau27@gmail.com>
2024-04-18 16:50:09 -0400

    realizadas las consultas. Para ver el resultado hay que descomentar cada instruccion. Para elaborar la base de datos hay que copiar lo que viene en el readme

--- index.js
+++ index.js
@@ -1,32 +1,75 @@
 // Importamos la librería de PostgreSQL
-const { Client } = require("pg");
+const { Pool } = require('pg');
+
 
 // Función para conectarnos a la base de datos
 const connection = async () => {
-  // Capturamos los datos de Database, user y password desde la línea de comandos
-/*
-  const database = process.argv[2];
-  const user = process.argv[3];
-  const password = process.argv[4];
-*/
-  // Creamos una instancia de la clase Client con los datos capturados
-  const client = new Client({
-    host: "localhost",
+  // Creamos una instancia de la clase pool
+//***CREAMOS EL OBJETO DE CONFIGURACIÓN***
+const config = {
+    host: 'localhost',
+    database: 'always_music',
+    user: 'pmaureira',
+    password: 'M4ur3ir4!!Aa',
     port: 5432,
-    database: database,
-    user: user,
-    password: password,
-  });
-
+   }
+//PASAMOS EL OBJETO AL CONSTRUCTOR DE LA CLASE POOL
+const pool = new Pool(config );
   // Nos conectamos a la base de datos
-  await client.connect();
+  await pool.connect();
 
-  // Realizamos una consulta a la base de datos
-  const result = await client.query("SELECT * FROM users");
+
+  //***PARA CORRER EL CODIGO HAY QUE DESCOMENTAR CADA INSTRUCCIÓN***/
+  
+/*
+  // Realizamos una consulta a la base de datos que muestre MUESTRA LA TABLA alumnos
+  const alumnos = await pool.query("SELECT * FROM alumnos");
   // Mostramos el resultado de la consulta
-  console.log(result.rows);
+  console.log(alumnos.rows);
   // Cerramos la conexión a la base de datos
-  await client.end();
+  await pool.end();
+*/
+  
+/*
+  // Realizamos una consulta a la base de datos que muestre la información del RUT '16.789.202-4' en este caso el alumno Sergio Mora
+  const alumno_rut = await pool.query("SELECT * FROM alumnos WHERE rut = '16.789.202-4' ");
+  // Mostramos el resultado de la consulta
+  console.log(alumno_rut.rows);
+  // Cerramos la conexión a la base de datos
+  await pool.end();
+*/
+
+/*
+  // Realizamos una consulta a la base de datos EDITANDO un alumno 
+
+  const editar_alumno = await pool.query("UPDATE alumnos SET curso ='Guitarra' WHERE curso='Guitarra y Bajo';");
+  // Mostramos el resultado de la consulta
+  console.log(editar_alumno.rows);
+  const alumnos = await pool.query("SELECT * FROM alumnos");
+  console.log(alumnos.rows);
+  // Cerramos la conexión a la base de datos
+  await pool.end();
+*/
+
+/*
+//Realizamos la consulta a la base AGREGANDO un Alumno nuevo
+  const agregar_alumno = await pool.query("INSERT INTO alumnos (nombre, rut, curso, nivel) VALUES ('Mercedes Gamboa', '12.587.039-7', 'Canto', 10);");
+// Mostramos el resultado de la consulta
+  console.log(alumnos.rows);
+// Cerramos la conexión a la base de datos
+  await pool.end();
+*/
+
+/*
+// Realizamos una consulta a la base de datos BORRANDO a un alumno rut = '14.745.441-1'
+  const result = await pool.query("DELETE from alumnos WHERE rut ='14.745.441-1';");
+// Mostramos la nueva tabla resultado de la consulta
+  console.log(alumnos.rows);
+// Cerramos la conexión a la base de datos
+  await pool.end();
+*/
+
+
 };
 
 // Llamamos a la función connection
